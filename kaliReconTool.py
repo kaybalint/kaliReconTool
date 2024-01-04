97756f1714d9ce2474f5b9048f93f56c33dde149
Krista Balint <94645482+kaybalint@users.noreply.github.com>
2024-01-04 14:09:29 -0500

    Update kaliReconTool.py

--- kaliReconTool.py
+++ kaliReconTool.py
@@ -19,7 +19,7 @@ def main():
     else:
         filename = "Scan_Results_" + target
     results = open(filename, 'w')
-    results.write(f"Target: {target}\n")
+    results.write(f"Target: {target}\n\n")
     results.write("*"*20+"\n")
     nmap = nmapScan(target)
     addToFile("NMAP SCAN", results, nmap)
@@ -31,10 +31,14 @@ def nmapScan(target):
     nm = nmap.PortScanner()
     nm.scan(hosts = target, arguments = "-p- -A")
     print("Nmap scan complete.")
-    return nm.csv()
+    data = nm.csv()
+    nmapData = ""
+    for line in data.split("\n"):
+        nmapData+=line.replace(";","\t|\t")+"\n"
+    return nmapData
     
 def addToFile(scan, file, results):
-    file.write(scan+"\n")
+    file.write(scan+"\n\n")
     file.write(results+"\n")
     file.write("*"*20+"\n")
 
