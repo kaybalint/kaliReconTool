b8240699118eab5be0bece3a7015b7a5b37b7879
Krista Balint <94645482+kaybalint@users.noreply.github.com>
2024-01-04 13:29:54 -0500

    Update kaliReconTool.py

--- kaliReconTool.py
+++ kaliReconTool.py
@@ -12,20 +12,24 @@ def main():
     print(f'Testing {target}...')
     filename = "Scan Results for " + target
     results = open(filename, 'w')
-
-    nm = nmap.PortScanner()
-    nm.scan(hosts = target, arguments = "-p- -A")
-    addToFile("Nmap scan", results, nm.csv())
-    print("Nmap scan complete...")
-
+    results.write(f"Target: {target}\n")
+    results.write("*"*20+"\n")
+    nmap = nmapScan(target)
+    addToFile("Nmap scan", results, nmap)
     results.close()
     print(f"Results are located in {filename}.")
 
-
+def nmapScan(target):
+    print("Starting Nmap scan...")
+    nm = nmap.PortScanner()
+    nm.scan(hosts = target, arguments = "-p- -A")
+    print("Nmap scan complete.")
+    return nm.csv()
+    
 def addToFile(scan, file, results):
-    file.write(scan)
+    file.write(scan+"\n")
     file.write(results)
-    file.write("*"*10)
+    file.write("*"*20)
 
 if __name__ == "__main__":
     main()
