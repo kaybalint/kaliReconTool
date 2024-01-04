6c2727e4a5208682f5ecf7cd0f708e40ca16f17d
Krista Balint <94645482+kaybalint@users.noreply.github.com>
2024-01-04 16:16:42 -0500

    Update kaliReconTool.py

--- kaliReconTool.py
+++ kaliReconTool.py
@@ -22,20 +22,27 @@ def main():
         filename = "Scan_Results_" + target
     results = open(filename, 'w')
     results.write(f"Target: {target}\nStart Time: {datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d %H:%M:%S')}\n\n")
-    results.write("*"*20+"\n\n")
-    nmap = nmapScan(target)
-    addToFile("NMAP SCAN", results, nmap)
+    results.write("*"*20+"\n")
+    nmap = nmapScan(target, results)
     results.write(f"Scan complete.\nEnd Time: {datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d %H:%M:%S')}")
     results.close()
     print(f"Results are located in {filename}.")
 
-def nmapScan(target):
+def nmapScan(target, file):
     print("Starting Nmap scan...")
+    file.write("NMAP SCAN\nStart Time: {datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d %H:%M:%S')}\n"
     nm = nmap.PortScanner()
     nm.scan(hosts = target, arguments = "-p- -A")
     print("Nmap scan complete.")
-    data = nm.csv()
-    nmapData = ""
+    file.write("End Time: {datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d %H:%M:%S')}\n\n"
+    for host in nm.all_hosts():
+        file.write(f"Host: {host} ({nm[host].hostname()})\n")
+        file.write(f"State: {nm[host].state()}\n----------n")
+        for protocol in nm[host].all_protocols():
+               file.write(f"Protocol: {protocol}\n")
+               for port in nm[host][protocol].keys():
+                   print(f"Port: {port}\tState: {nm[host][portocol][port]['state']}\tProduct: {nm[host][portocol][port]['product']}\t
+               
     for line in data.split("\n"):
         nmapData+=line.replace(";","  |  ")+"\n"
     return nmapData
