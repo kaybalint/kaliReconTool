1c4242efe733319f0609e0beb2bf4bf620af7c81
Krista Balint <94645482+kaybalint@users.noreply.github.com>
2024-01-04 10:03:31 -0500

    Update kaliReconTool.py

--- kaliReconTool.py
+++ kaliReconTool.py
@@ -1,7 +1,7 @@
 import sys
 
-if len(sys.argv) == 0:
+if len(sys.argv) == 1:
     ip = input('Enter IP address: ')
     print(f'Testing {ip}...')
-elif len(sys.argv) == 1:
+elif len(sys.argv) == 2:
     print(f'Testing {sys.argv[0]}...')
