ee5de2893d368df188b77f7e292bab1242a8dd0e
Krista Balint <94645482+kaybalint@users.noreply.github.com>
2024-01-04 13:16:17 -0500

    Update kaliReconTool.py

--- kaliReconTool.py
+++ kaliReconTool.py
@@ -2,25 +2,30 @@ import sys
 import re
 import nmap
 
-if len(sys.argv) == 1:
-    target = input('Enter target IP or URL: ')
-    
-elif len(sys.argv) == 2:
-    target = sys.argv[1]
+def main():
+    if len(sys.argv) == 1:
+        target = input('Enter target IP or URL: ')
+        
+    elif len(sys.argv) == 2:
+        target = sys.argv[1]
 
-print(f'Testing {target}...')
-filename = "Scan Results for " + target
-results = open(filename, 'w')
+    print(f'Testing {target}...')
+    filename = "Scan Results for " + target
+    results = open(filename, 'w')
 
-nm = nmap.PortScanner()
-nm.scan(hosts = target, arguments = "-p- -A")
-addToFile("Nmap scan", results, nm.csv())
-print("Nmap scan complete...")
+    nm = nmap.PortScanner()
+    nm.scan(hosts = target, arguments = "-p- -A")
+    addToFile("Nmap scan", results, nm.csv())
+    print("Nmap scan complete...")
 
-results.close()
+    results.close()
+    print(f"Results are located in {filename}.")
+
 
 def addToFile(scan, file, results):
     file.write(scan)
     file.write(results)
     file.write("*"*10)
 
+if __name__ == "__main__":
+    main()
