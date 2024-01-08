41b7cfc189c5fdcae93ce456319bfea65ec878a5
Krista Balint <94645482+kaybalint@users.noreply.github.com>
2024-01-08 10:15:58 -0500

    Update kaliReconTool.py

--- kaliReconTool.py
+++ kaliReconTool.py
@@ -35,23 +35,31 @@ def main():
 
 def nmapScan(target, file):
     print("Starting Nmap scan...")
+    print(f"NMAP SCAN\nStart Time: {datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d %H:%M:%S')}\n")
     file.write(f"NMAP SCAN\nStart Time: {datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d %H:%M:%S')}\n")
     nm = nmap.PortScanner()
     nm.scan(hosts = target, arguments = "-p- -A")
     print("Nmap scan complete.\n")
+    print(f"End Time: {datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d %H:%M:%S')}\n\n")
     file.write(f"End Time: {datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d %H:%M:%S')}\n\n")
     for host in nm.all_hosts():
+        print(f"Host: {host} ({nm[host].hostname()})\n")
         file.write(f"Host: {host} ({nm[host].hostname()})\n")
+        print(f"State: {nm[host].state()}\n----------\n")
         file.write(f"State: {nm[host].state()}\n----------\n")
         for protocol in nm[host].all_protocols():
+               print(f"Protocol: {protocol}\n")
                file.write(f"Protocol: {protocol}\n")
                for port in nm[host][protocol].keys():
+                   print(f"Port: {port}\t")
                    file.write(f"Port: {port}\t")
                    for data in nm[host][protocol][port].keys():
                        x = nm[host][protocol][port][data]
                        if x:
+                           print(f"{data.capitalize()}: {x}\t")
                            file.write(f"{data.capitalize()}: {x}\t")
                        else:
+                           print(f"{data.capitalize()}: N/A\t")
                            file.write(f"{data.capitalize()}: N/A\t")
                    file.write("\n\n")
     file.write("*"*20+"\n\n")
@@ -60,27 +68,32 @@ def niktoScan(target, file):
     print("Starting Nikto scan...")
     file.write("NIKTO SCAN\n")
     nk = subprocess.run(["nikto", "-h", target], capture_output=True)
-    for line in nk.stdout.decode('UTF-8').split("\n"):
-        file.write(line+"\n")
     print("Nikto scan complete.\n")
+    for line in nk.stdout.decode('UTF-8').split("\n"):
+        print(line)
+        file.write(line+"\n")
     file.write("*"*20+"\n\n")
 
 def dirbScan(target, file):
     print("Starting Dirb scan...")
     file.write("DIRB SCAN\n")
     nk = subprocess.run(["dirb", "", target], capture_output=True)
-    for line in nk.stdout.decode('UTF-8').split("\n"):
-        file.write(line+"\n")
     print("Dirb scan complete.\n")
+    for line in nk.stdout.decode('UTF-8').split("\n"):t
+        print(line)
+        file.write(line+"\n")
+    print("*"*20+"\n\n")
     file.write("*"*20+"\n\n")
 
 def sslScan(target, file):
     print("Starting SSL scan...")
     file.write("SSL SCAN\n")
     nk = subprocess.run(["sslscan", "", target], capture_output=True)
-    for line in nk.stdout.decode('UTF-8').split("\n"):
-        file.write(line+"\n")
     print("SSL scan complete.\n")
+    for line in nk.stdout.decode('UTF-8').split("\n"):
+        print(line)
+        file.write(line+"\n")
+    print("*"*20+"\n\n")
     file.write("*"*20+"\n\n")
 
 if __name__ == "__main__":
